1de4fb422f79724a069d811bf44391636f90c6ae
Sudhanshu069 <singhsudhanshu069@gmail.com>
2022-06-22 19:19:31 +0530

    learn sieve

---
+++ maths/maths-sieve-of-eratosthenes.cpp
@@ -0,0 +1,14 @@
+/*
+PROBLEM: Given a number, we have to find prime numbers upto that number
+*/
+
+/*
+LOGIC: 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+void sieve(int n){
+    vector<bool> isPrime(n + 1, true);
+    
+}
