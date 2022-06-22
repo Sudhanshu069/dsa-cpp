5c22c000c0b808ec42e07194168d1e07b8538165
Sudhanshu069 <singhsudhanshu069@gmail.com>
2022-06-22 19:17:26 +0530

    learn to find lcm of two numbers

---
+++ maths/maths-lcm-two-numbers.cpp
@@ -0,0 +1,23 @@
+/*
+PROBLEM: find lcm of two numbers
+*/
+
+/*
+LOGIC: we know that: a*b = gcd(a,b) * lcm(a,b)
+
+time complexity: O(log(min(a,b)))
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b){
+    if(b == 0){
+        return a;
+    }
+    return gcd(b, a%b);
+}
+
+int lcm(int a, int b){
+    return (a * b)/gcd(a,b);
+}
